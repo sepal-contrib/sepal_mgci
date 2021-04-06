715f10cac26416f653476eb5c3a975ed109a4fe9
ingdanielguerrero <dfgm2006@gmail.com>
2021-04-06 21:22:09 +0000

    created decorator to catch loading functions

--- component/scripts/decorators.py
+++ component/scripts/decorators.py
@@ -1,18 +1,18 @@
 import functools
 
 # Definition of decorator that is receiving two arguments
-def loading(btn, alert):
+def loading(alert, btn=None):
     def decorator_loading(func):
         @functools.wraps(func)
         def wrapper_loading(*args, **kwargs):
-            btn.loading=True
+            if btn : btn.loading=True
             try:
                 value = func(*args, **kwargs)
             except Exception as e:
-                btn.loading=False
+                if btn : btn.loading=False
                 alert.add_msg(f'{e}', type_='error')
                 raise e
-            btn.loading=False
+            if btn : btn.loading=False
             return value
         return wrapper_loading
     return decorator_loading
