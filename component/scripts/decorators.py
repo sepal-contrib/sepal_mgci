7892b4c4838e216227d1ca905a87fc2aa303de0e
ingdanielguerrero <dfgm2006@gmail.com>
2021-04-14 23:05:03 +0000

    refactor decorator

--- component/scripts/decorators.py
+++ component/scripts/decorators.py
@@ -1,7 +1,16 @@
 import functools
+import sepal_ui.sepalwidgets as sw
+
 
 # Definition of decorator that is receiving two arguments
-def loading(alert, dialog, btn=None):
+def loading(dialog, btn=None):
+    """Decorator to toggle loading a button and display errors on a dialog
+    
+    Args:
+        dialog (Dialog) : Custom dialog with Output children
+        btn (sw.Btn, v.Btn) : Event button 
+    """
+    
     def decorator_loading(func):
         @functools.wraps(func)
         def wrapper_loading(*args, **kwargs):
@@ -9,11 +18,17 @@ def loading(alert, dialog, btn=None):
             try:
                 value = func(*args, **kwargs)
             except Exception as e:
+                
                 if btn : btn.loading=False
-                alert.add_msg(f'{e}', type_='error')
-                dialog.alert(alert)
-                raise e
+                dialog.v_model = True
+                
+                with dialog.output:
+                    display(sw.Alert().add_msg(f'{e}', type_='error').show())
+                    
             if btn : btn.loading=False
-            return value
+
         return wrapper_loading
     return decorator_loading
+
+# Necesito el output como children del dialog
+# Luego dentro del decorador puedo limpiar el dialog y mostrar el aviso
