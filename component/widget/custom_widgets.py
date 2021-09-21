16202581d72f0599d8916fc0ee9755ded5ecefa7
dfguerrerom <dfgm2006@gmail.com>
2021-09-21 19:41:32 +0000

    feat: create a bool questionaire widget

---
+++ component/widget/custom_widgets.py
@@ -0,0 +1,30 @@
+from traitlets import link, CBool
+import ipyvuetify as v
+import sepal_ui.sepalwidgets as sw
+
+__all__ = ['BoolQuestion']
+
+
+class BoolQuestion(v.Flex, sw.SepalWidget):
+    
+    v_model = CBool(False).tag(sync=True)
+    
+    def __init__(self, question, *args, **kwargs):
+        
+        super().__init__(*args, **kwargs)
+        
+        bool_radios = v.RadioGroup(
+            row=True,
+            v_model=self.v_model,
+            children=[
+                v.Radio(label='No', value=False),
+                v.Radio(label='Yes', value=True),
+            ]
+        )
+        
+        link((bool_radios, 'v_model'), (self, 'v_model'))
+        
+        self.children = [
+            v.Html(tag='h3', children=[question]),
+            bool_radios
+        ]
