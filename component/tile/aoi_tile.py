6d73731b60f2b627124b86152fe56a08c3a44ada
dfguerrerom <dfgm2006@gmail.com>
2022-11-04 16:48:40 +0000

    feat: refactor aoi view and tile to display bioclimatic belts

--- component/tile/aoi_tile.py
+++ component/tile/aoi_tile.py
@@ -1,39 +1,65 @@
-from sepal_ui import aoi
+import ee
+
+ee.Initialize()
+
 import pandas as pd
+import sepal_ui.sepalwidgets as sw
+from sepal_ui.mapping import SepalMap
 
 import component.parameter as param
+from component.tile.aoi_view import AoiView
+from component.widget.legend_control import LegendControl
 
-__all__ = ["aoi_tile"]
+__all__ = ["AoiTile"]
 
-# Define area of interest tile
-aoi_tile = aoi.AoiTile(methods=["-POINTS", "-DRAW"])
 
-# Rename selection methos as referenced in:
-# https://github.com/dfguerrerom/sepal_mgci/issues/7
+class AoiTile(sw.Layout):
+    """Custo AOI Tile"""
 
-aoi_tile.view.w_method.items = param.CUSTOM_AOI_ITEMS
-aoi_view = aoi_tile.view
-aoi_model = aoi_view.model
+    def __init__(self, methods="ALL", gee=True):
 
-# Display only the countries that matches with m49
+        super().__init__()
 
-# Read m49 countries.
-m49_countries = pd.read_csv(param.M49_FILE, sep=";")
+        # create the map
+        self.map_ = SepalMap(dc=True, gee=gee)
+        self.map_.dc.hide()
 
-# Read AOI gaul dataframe
-gaul_dataset = (
-    pd.read_csv(aoi_model.FILE[1])
-    .drop_duplicates(subset=aoi_model.CODE[1].format(0))
-    .sort_values(aoi_model.NAME[1].format(0))
-    .rename(columns={"ISO 3166-1 alpha-3": "iso31661"})
-)
+        self.view = AoiView(
+            map_=self.map_,
+            methods=["-POINTS", "-DRAW"],
+        )
 
-# Get only the gaul contries present in the m49
-m49_dataset = gaul_dataset[gaul_dataset.iso31661.isin(m49_countries.iso31661)]
-gaul_codes = m49_dataset.ADM0_CODE.to_list()
+        # Rename selection methos as referenced in:
+        # https://github.com/dfguerrerom/sepal_mgci/issues/7
+        self.view.w_method.items = param.CUSTOM_AOI_ITEMS
+        self.view.w_admin_0.items = self.get_m49()
 
-# Create the new items
-m49_items = [item for item in aoi_view.w_admin_0.items if item["value"] in gaul_codes]
+        self.children = [
+            sw.Flex(xs12=True, md5=True, class_="pa-5", children=[self.view]),
+            sw.Flex(xs12=True, md7=True, class_="pa-1", children=[self.map_]),
+        ]
 
-# Replace them.
-aoi_view.w_admin_0.items = m49_items
+    def get_m49(self):
+        """Display only the countries that matches with m49"""
+
+        # Read m49 countries.
+        m49_countries = pd.read_csv(param.M49_FILE, sep=";")
+
+        # Read AOI gaul dataframe
+        gaul_dataset = (
+            pd.read_csv(self.view.model.FILE[1])
+            .drop_duplicates(subset=self.view.model.CODE[1].format(0))
+            .sort_values(self.view.model.NAME[1].format(0))
+            .rename(columns={"ISO 3166-1 alpha-3": "iso31661"})
+        )
+
+        # Get only the gaul contries present in the m49
+        m49_dataset = gaul_dataset[gaul_dataset.iso31661.isin(m49_countries.iso31661)]
+        gaul_codes = m49_dataset.ADM0_CODE.to_list()
+
+        # Subset new items
+        m49_items = [
+            item for item in self.view.w_admin_0.items if item["value"] in gaul_codes
+        ]
+
+        return m49_items
---
+++ component/tile/aoi_view.py
@@ -0,0 +1,241 @@
+from datetime import datetime as dt
+
+import sepal_ui.sepalwidgets as sw
+from sepal_ui.aoi.aoi_model import AoiModel
+from sepal_ui.aoi.aoi_view import AdminField, MethodSelect
+from sepal_ui.message import ms
+from sepal_ui.scripts import utils as su
+from traitlets import Int
+
+from component.scripts.biobelt import add_belt_map
+
+
+class AoiView(sw.Layout):
+    """
+    Versatile card object to deal with the aoi selection. multiple selection method are available (see the MethodSelector object) and the widget can be fully customizable. Can also be bound to ee (ee==True) or not (ee==False)
+
+    Args:
+        methods (list, optional): the methods to use in the widget, default to 'ALL'. Available: {'ADMIN0', 'ADMIN1', 'ADMIN2', 'SHAPE', 'DRAW', 'POINTS', 'ASSET', 'ALL'}
+        map\_ (SepalMap, optional): link the aoi_view to a custom SepalMap to display the output, default to None
+        gee (bool, optional): wether to bind to ee or not
+        vector (str|pathlib.Path, optional): the path to the default vector object
+        admin (int, optional): the administrative code of the default selection. Need to be GADM if :code:`ee==False` and GAUL 2015 if :code:`ee==True`.
+        asset (str, optional): the default asset. Can only work if :code:`ee==True`
+    """
+
+    # ##########################################################################
+    # ###                             widget parameters                      ###
+    # ##########################################################################
+
+    updated = Int(0).tag(sync=True)
+    "int: traitlets triggered every time a AOI is selected"
+
+    ee = True
+    "bool: either or not he aoi_view is connected to gee"
+
+    folder = None
+    "str: the folder name used in GEE related component, mainly used for debugging"
+
+    model = None
+    "sepal_ui.aoi.AoiModel: the model to create the AOI from the selected parameters"
+
+    # ##########################################################################
+    # ###                            the embeded widgets                     ###
+    # ##########################################################################
+
+    map_ = None
+    "sepal_ui.mapping.SepalMap: the map to draw the AOI"
+
+    w_method = None
+    "widget: the widget to select the method"
+
+    components = None
+    "dict: the followingwidgets used to define AOI"
+
+    w_admin_0 = None
+    "widget: the widget used to select admin level 0"
+
+    w_admin_1 = None
+    "widget: the widget used to select admin level 1"
+
+    w_admin_2 = None
+    "widget: the widget used to select admin level 2"
+
+    w_vector = None
+    "widget: the widget used to select vector shapes"
+
+    w_points = None
+    "widget: the widget used to select points files"
+
+    w_draw = None
+    "widget: the widget used to select the name of a drawn shape (only if :code:`map_ != None`)"
+
+    w_asset = None
+    "widget: the widget used to select asset name of a featureCollection (only if :code:`gee == True`)"
+
+    btn = None
+    "sw.Btn: a default btn"
+
+    alert = None
+    "sw.Alert: a alert to display message to the end user"
+
+    def __init__(
+        self, methods="ALL", map_=None, gee=True, folder=None, model=None, **kwargs
+    ):
+        self.class_ = "d-block"
+
+        # set ee dependencie
+        self.ee = gee
+        self.folder = folder
+
+        gee is False or su.init_ee()
+
+        # get the model
+        self.model = model or AoiModel(gee=gee, folder=folder, **kwargs)
+
+        # get the map if filled
+        self.map_ = map_
+
+        # create the method widget
+        self.w_method = MethodSelect(methods, gee=gee, map_=map_)
+
+        # add the methods blocks
+        self.w_admin_0 = AdminField(0, gee=gee).get_items()
+        self.w_admin_1 = AdminField(1, self.w_admin_0, gee=gee)
+        self.w_admin_2 = AdminField(2, self.w_admin_1, gee=gee)
+        self.w_vector = sw.VectorField(label=ms.aoi_sel.vector)
+        self.w_points = sw.LoadTableField(label=ms.aoi_sel.points)
+        self.w_draw = sw.TextField(label=ms.aoi_sel.aoi_name)
+
+        # group them together with the same key as the select_method object
+        self.components = {
+            "ADMIN0": self.w_admin_0,
+            "ADMIN1": self.w_admin_1,
+            "ADMIN2": self.w_admin_2,
+            "SHAPE": self.w_vector,
+            "POINTS": self.w_points,
+            "DRAW": self.w_draw,
+        }
+
+        # hide them all
+        [c.hide() for c in self.components.values()]
+
+        # use the same alert as in the model
+        self.alert = sw.Alert()
+
+        # bind the widgets to the model
+        (
+            self.model.bind(self.w_admin_0, "admin")
+            .bind(self.w_admin_1, "admin")
+            .bind(self.w_admin_2, "admin")
+            .bind(self.w_vector, "vector_json")
+            .bind(self.w_points, "point_json")
+            .bind(self.w_method, "method")
+            .bind(self.w_draw, "name")
+        )
+
+        # defint the asset select separately. If no gee is set up we don't want any
+        # gee based widget to be requested. If it's the case, application that does
+        # not support GEE
+        # will crash if the user didn't authenticate
+        if self.ee:
+            self.w_asset = sw.VectorField(
+                label=ms.aoi_sel.asset, gee=True, folder=self.folder, types=["TABLE"]
+            ).hide()
+            self.components["ASSET"] = self.w_asset
+            self.model.bind(self.w_asset, "asset_name")
+
+        # add a validation btn
+        self.btn = sw.Btn(ms.aoi_sel.btn)
+
+        # create the widget
+        self.children = (
+            [self.w_method] + [*self.components.values()] + [self.btn, self.alert]
+        )
+
+        super().__init__(**kwargs)
+
+        # js events
+        self.w_method.observe(self._activate, "v_model")  # activate widgets
+        self.btn.on_event("click", self._update_aoi)  # load the informations
+
+        # reset te aoi_model
+        self.model.clear_attributes()
+
+    @su.loading_button(debug=True)
+    def _update_aoi(self, widget, event, data):
+        """load the object in the model & update the map (if possible)"""
+
+        # read the information from the geojson datas
+        if self.map_:
+            self.map_.remove_all()
+            self.model.geo_json = self.map_.dc.to_json()
+
+        if hasattr(self.map_, "legend"):
+            self.map_.legend.legend_dict = {}
+
+        # update the model
+        self.model.set_object()
+
+        # update the map
+        if self.map_:
+            self.map_.remove_layer("aoi", none_ok=True)
+            self.map_.zoom_bounds(self.model.total_bounds())
+
+            if self.ee:
+                self.map_.add_ee_layer(self.model.feature_collection, {}, name="aoi")
+            else:
+                self.map_.add_layer(self.model.get_ipygeojson())
+
+            self.map_.hide_dc()
+
+        add_belt_map(self.model, self.map_)
+
+        # tell the rest of the apps that the aoi have been updated
+        self.updated += 1
+
+        self.alert.add_msg(ms.aoi_sel.complete, "success")
+
+        return self
+
+    def reset(self):
+        """clear the aoi_model from input and remove the layer from the map (if existing)"""
+
+        # reset the view of the widgets
+        self.w_method.v_model = None
+
+        # clear the map
+        self.map_ is None or self.map_.remove_layer("aoi", none_ok=True)
+
+        return self
+
+    @su.switch("loading", on_widgets=["w_method"])
+    def _activate(self, change):
+        """activate the adapted widgets"""
+
+        # clear and hide the alert
+        self.alert.reset()
+
+        # hide the widget so that the user doens't see status changes
+        [w.hide() for w in self.components.values()]
+
+        # clear the inputs in a second step as reseting a FileInput can be long
+        [w.reset() for w in self.components.values()]
+
+        # deactivate or activate the dc
+        # clear the geo_json saved features to start from scratch
+        if self.map_:
+            if change["new"] == "DRAW":
+                self.map_.dc.show()
+            else:
+                self.map_.dc.hide()
+
+        # activate the correct widget
+        w = next((w for k, w in self.components.items() if k == change["new"]), None)
+        w is None or w.show()
+
+        # init the name to the current value
+        now = dt.now().strftime("%Y-%m-%d_%H-%M-%S")
+        self.w_draw.v_model = None if change["new"] is None else f"Manual_aoi_{now}"
+
+        return
