c257535dbf780d5b636c9f3e741783e185e80e0d
dfguerrerom <dfgm2006@gmail.com>
2023-04-05 23:30:35 +0000

    refactor: modify columns used for sub_a indicator and add cols for mountain area table

--- component/parameter/index_parameters.py
+++ component/parameter/index_parameters.py
@@ -1,42 +1,112 @@
-sub_a_cols = [
+mountain_area_cols = [
+    "Indicator",
     "SeriesID",
-    "SeriesDescription",
-    "GeoAreaCode",
+    "SERIES",
+    "SeriesDesc",
+    "REF_AREA",
     "GeoAreaName",
-    "TimePeriod",
-    "Value",
-    "Time_Detail",
-    "Source",
-    "FootNote",
-    "Nature",
-    "Units",
-    "Reporting Type",
-    "Observation Status",
-    "Bioclimatic Belt",
-    "ISOalpha3",
-    "Type",
-    "SeriesCode",
+    "TIME_PERIOD",
+    "TIME_DETAIL",
+    "SOURCE_DETAIL",
+    "COMMENT_OBS",
+    "NATURE",
+    "OBS_STATUS",
+    "UNIT_MEASURE",
+    "UNIT_MULT",
+    "BIOCLIMATIC_BELT",
+    "OBS_VALUE",
+    "OBS_VALUE_RSA",
 ]
 
-sub_a_landtype_cols = [
+# sub_a_cols = [
+#     "SeriesID",
+#     "SeriesDescription",
+#     "GeoAreaCode",
+#     "GeoAreaName",
+#     "TimePeriod",
+#     "Value",
+#     "Time_Detail",
+#     "Source",
+#     "FootNote",
+#     "Nature",
+#     "Units",
+#     "Reporting Type",
+#     "Observation Status",
+#     "Bioclimatic Belt",
+#     "ISOalpha3",
+#     "Type",
+#     "SeriesCode",
+# ]
+
+sub_a_cols = [
+    "Indicator",
     "SeriesID",
-    "SeriesDescription",
-    "GeoAreaCode",
+    "SERIES",
+    "SeriesDesc",
+    "REF_AREA",
     "GeoAreaName",
-    "TimePeriod",
-    "Value",
-    "Time_Detail",
-    "Source",
-    "FootNote",
-    "Nature",
-    "Units",
-    "Reporting Type",
-    "Observation Status",
-    "Bioclimatic Belt",
-    "Land Type",
-    "ISOalpha3",
-    "Type",
-    "SeriesCode",
+    "TIME_PERIOD",
+    "TIME_DETAIL",
+    "SOURCE_DETAIL",
+    "COMMENT_OBS",
+    "NATURE",
+    "OBS_STATUS",
+    "UNIT_MEASURE",
+    "UNIT_MULT",
+    "BIOCLIMATIC_BELT",
+    # "LAND_COVER",
+    "OBS_VALUE",
+]
+
+# This shows the tracebility of the columns from the previous version to the new
+# sub_a_landtype_cols = [
+#     "Indicator",  # new
+#     "SeriesID",
+#     "SERIES",  # new
+#     "SeriesDescription",  # change to SeriesDesc
+#     "GeoAreaCode",  # Change to REF_AREA
+#     "GeoAreaName",
+#     "TimePeriod",  # uppercase
+#     "Time_Detail",  # uppercase
+#     "SOURCE_DETAIL",  # new
+#     "COMMENT_OBS",  # new
+#     "NATURE",  # new
+#     "OBS_STATUS",  # new
+#     "Units",  # change to UNIT_MEASURE
+#     "UNIT_MULT",  # new
+#     "Bioclimatic Belt",  # change to BIOCLIMATIC_BELT
+#     "Land Type",  # change to LAND_COVER
+#     "Value",  # change to OBS_VALUE
+#     "OBS_VALUE_RSA",  # new
+#     "Source",  # Remove
+#     "FootNote",  # Remove
+#     "Nature",  # Remove
+#     "Reporting Type",  # Remove
+#     "Observation Status",  # Remove
+#     "ISOalpha3",  # Remove
+#     "Type",  # Remove
+#     "SeriesCode",  # Remove
+# ]
+
+sub_a_landtype_cols = [
+    "Indicator",
+    "SeriesID",
+    "SERIES",
+    "SeriesDesc",
+    "REF_AREA",
+    "GeoAreaName",
+    "TIME_PERIOD",
+    "TIME_DETAIL",
+    "SOURCE_DETAIL",
+    "COMMENT_OBS",
+    "NATURE",
+    "OBS_STATUS",
+    "UNIT_MEASURE",
+    "UNIT_MULT",
+    "BIOCLIMATIC_BELT",
+    "LAND_COVER",
+    "OBS_VALUE",
+    "OBS_VALUE_RSA",
 ]
 
 sub_b_perc_cols = [
@@ -58,7 +128,6 @@ sub_b_perc_cols = [
     "Type",
     "SeriesCode",
 ]
-
 sub_b_landtype_cols = [
     "SeriesID",
     "SeriesDescription",
