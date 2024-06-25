fee4ed38c2ecf537e03dbc775c207e1de2cd2df9
dfguerrerom <dfgm2006@gmail.com>
2024-06-25 15:21:59 +0200

    test: set test for ui

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,9 @@ The nox run are build in isolated environment that will be stored in .nox. to fo
 """
 
 from pathlib import Path
-
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+from jupyter_client.kernelspec import KernelSpecManager
 import nox
 
 
@@ -31,6 +33,40 @@ def app(session):
     session.run(
         "voila", "--show_tracebacks=True", "--template=sepal-ui-base", "nox_ui.ipynb"
     )
+
+
+@nox.session()
+def test_ui(session):
+    """Run the application."""
+
+    ksm = KernelSpecManager()
+    kernel_names = list(ksm.get_all_specs())
+    print(kernel_names)
+
+    # get the current path
+    root_folder = Path(__file__).parent
+    repo_name = root_folder.name
+
+    # Copy the ui.ipynb to test_ui.ipynb
+    session.run("cp", root_folder / "ui.ipynb", root_folder / "nox_ui.ipynb")
+
+    session.run("entry_point", "--test", root_folder / "nox_ui.ipynb")
+
+    test_notebooks = [root_folder / "nox_ui.ipynb"]
+
+    for notebook in test_notebooks:
+        with open(notebook) as ff:
+            nb_in = nbformat.read(ff, nbformat.NO_CONVERT)
+
+        print("Running notebook", notebook)
+
+        try:
+            ep = ExecutePreprocessor(timeout=600, kernel_name=f"test-{repo_name}")
+
+            nb_out = ep.preprocess(nb_in)
+        except Exception as e:
+            print("########### Error running notebook", notebook)
+            raise e
 
 
 @nox.session(reuse_venv=True)
