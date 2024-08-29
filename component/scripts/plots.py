1248f6d2ccad223fa9a84a4f50229d660d91df1c
dfguerrerom <dfgm2006@gmail.com>
2024-08-29 16:37:08 -0700

    refactor: use ipecharts and replace old sankey

--- component/scripts/plots.py
+++ component/scripts/plots.py
@@ -1,15 +1,111 @@
-# -*- coding: utf-8 -*-
-"""
-Produces Sankey Diagrams with matplotlib to show the transition in land cover.
-adopted from : Sankey by  Anneya Golob & marcomanz & pierre-sassoulas & jorwoods
-from https://github.com/anazalea/pySankey
-"""
+from typing import Tuple
 from component.parameter.module_parameter import transition_degradation_matrix
 from collections import defaultdict
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
+
+def get_sankey_chart():
+    from ipecharts.option import Option, Legend, Tooltip
+    from ipecharts.option.series import Sankey
+    from ipecharts.echarts import EChartsWidget, EChartsRawWidget
+
+    sankey_data = Sankey(smooth=True, areaStyle={})
+    sankey_data.nodes = []
+    sankey_data.links = []
+    option = Option(series=[sankey_data], tooltip=Tooltip(), legend=Legend())
+
+    chart = EChartsWidget(option=option)
+
+    return sankey_data, chart
+
+
+def get_nodes_and_links(
+    df: pd.DataFrame,
+    lc_classes_path: str,
+    years: Tuple[str, str] = ["from_lc", "to_lc"],
+):
+    """Generate nodes and links for a Sankey diagram"""
+
+    from_lc, to_lc = [str(y) for y in years]
+
+    # Load labels dictionary
+    labels_df = pd.read_csv(lc_classes_path)
+
+    # Generate a dictionary to map land cover classes to descriptions and colors
+    desc_color_map = labels_df.set_index("lc_class")[["desc", "color"]].to_dict("index")
+
+    # Dictionary to store nodes and links by biobelt
+    biobelt_dict = {}
+
+    # Group the dataframe by 'biobelt'
+    grouped = df.groupby("belt_class")
+
+    # Process each biobelt group separately
+    for biobelt, group in grouped:
+        # Identify unique land cover classes within the biobelt group
+        unique_classes = np.unique(group[["from_lc", "to_lc"]].values)
+
+        # Generate nodes with unique land cover classes and their corresponding colors
+        nodes = []
+        for lc_class in unique_classes:
+            lc_info = desc_color_map.get(
+                lc_class, {"desc": "Unknown", "color": "#000000"}
+            )
+            lc_label = lc_info["desc"]
+            color = lc_info["color"]
+            nodes.extend(
+                {"name": f"{lc_label}_{year}", "itemStyle": {"color": color}}
+                for year in [from_lc, to_lc]
+            )
+
+        # Generate links based on the biobelt group
+        links = []
+        for _, row in group.iterrows():
+            source_info = desc_color_map.get(
+                row["from_lc"], {"desc": "Unknown", "color": "#000000"}
+            )
+            target_info = desc_color_map.get(
+                row["to_lc"], {"desc": "Unknown", "color": "#000000"}
+            )
+            links.append(
+                {
+                    "source": f"{source_info['desc']}_{from_lc}",
+                    "target": f"{target_info['desc']}_{to_lc}",
+                    "value": round(row["sum"], 2),  # Rounded to 2 decimal places
+                }
+            )
+
+        # Store nodes and links for this biobelt in the dictionary
+        biobelt_dict[biobelt] = {"nodes": nodes, "links": links}
+
+    return biobelt_dict
+
+
+def get_pyecharts_sankey(df: pd.DataFrame, lc_classes_path: str):
+    """Generate a Sankey diagram using Pyecharts
+
+    Args:
+        df (pd.DataFrame): A DataFrame with columns 'from', 'to', and 'sum'
+        lc_classes_path (str): Path to the land cover classes CSV file
+
+    Returns:
+        EChartsWidget: A Pyecharts EChartsWidget object
+    """
+
+    from ipecharts.option import Option, Legend, Tooltip
+    from ipecharts.option.series import Sankey
+    from ipecharts.echarts import EChartsWidget, EChartsRawWidget
+    import numpy as np
+
+    s = Sankey(smooth=True, areaStyle={})
+    s.data = nodes
+    s.links = links
+
+    option = Option(series=[s], tooltip=Tooltip(), legend=Legend())
+    return EChartsWidget(option=option)
 
 
 def sankey(df, colorDict=None, aspect=4, rightColor=False, fontsize=14):
--- component/tile/dashboard_tile.py
+++ component/tile/dashboard_tile.py
@@ -10,9 +10,15 @@ from component.model.model import MgciModel
 
 import component.parameter.module_parameter as param
 import component.scripts as cs
+from component.scripts.validation import validate_sankey_classes
 import component.widget as cw
 from component.message import cm
-from component.scripts.plots import sankey
+from component.scripts.plots import (
+    get_nodes_and_links,
+    get_pyecharts_sankey,
+    get_sankey_chart,
+    sankey,
+)
 from component.scripts.report_scripts import get_belt_desc
 from component.widget.map import MapView
 from component.widget.statistics_card import StatisticCard
@@ -32,6 +38,7 @@ class DashboardTile(sw.Card):
         map_view = MapView(self.model)
         dash_view_a = DashViewA(self.model)
         dash_view_b = DashViewB(self.model)
+        self.dash_view_b = dash_view_b
 
         dash_tabs = cw.Tabs(
             ["Visualization", "Sub indicator A", "Sub indicator B"],
@@ -71,13 +78,13 @@ class DashView(sw.Layout):
         self.clear()
         self.alert.add_msg(cm.dashboard.alert.rendering)
 
-        if not self.year_select.v_model:
-            raise Exception("Select a year.")
+        # if not self.year_select.v_model:
+        #     raise Exception("Select a year.")
 
-        if not self.model.results:
-            raise Exception(
-                "No results to display, go to the calculation step and perform the calculation first."
-            )
+        # if not self.model.results:
+        #     raise Exception(
+        #         "No results to display, go to the calculation step and perform the calculation first."
+        #     )
 
 
 class DashViewA(DashView):
@@ -162,23 +169,26 @@ class DashViewB(DashView):
         )
         self.belt_select = sw.Select(class_="mr-2", label="Select a belt", v_model=None)
 
-        self.btn = sw.Btn("Calculate", class_="ml-2")
+        self.sankey_data, self.chart = get_sankey_chart()
 
         self.children = [
             sw.Flex(
                 class_="d-flex align-center",
-                children=[self.year_select, self.belt_select, self.btn],
+                children=[self.year_select, self.belt_select],
             ),
+            self.chart,
             self.alert,
         ]
 
         # Observe reporting_years_{indicator} from model to update the year_select
 
         self.model.observe(self.set_years, "reporting_years_sub_b")
-        self.btn.on_event("click", self.render_dashboard)
         self.year_select.observe(self.set_belt_items, "v_model")
+        self.belt_select.observe(self.update_sankey_data, "v_model")
 
         self.set_years({"new": self.model.reporting_years_sub_b})
+
+        super().render_dashboard()
 
     def set_belt_items(self, change):
         """Set the belt items in the belt_select widget based on the year selected
@@ -191,14 +201,17 @@ class DashViewB(DashView):
 
         look_up_year = change["new"]
 
-        self.df = cs.parse_to_year(self.model.results, look_up_year)
+        df = cs.parse_to_year(self.model.results, look_up_year)
+        look_up_years = list(look_up_year.values())[0]
+        print(look_up_years)
+        self.nodes_and_links = get_nodes_and_links(df, param.LC_CLASSES, look_up_years)
 
         # Get all belts that are available for the selected year
 
         belt_items = [
             {"text": get_belt_desc(row), "value": row.belt_class}
             for _, row in pd.DataFrame(
-                self.df.belt_class.unique(), columns=["belt_class"]
+                df.belt_class.unique(), columns=["belt_class"]
             ).iterrows()
         ]
 
@@ -218,53 +231,11 @@ class DashViewB(DashView):
         else:
             self.year_select.items = []
 
-    @su.loading_button()
-    def render_dashboard(self, *args):
+    def update_sankey_data(self, change):
         """create the corresponding parsed dataframe based on the selected year.
         This dataframe will be used to calculate the MCGI"""
 
-        super().render_dashboard()
+        belt_data = self.nodes_and_links[change["new"]]
 
-        self.df_sankey = (
-            self.df[self.df.belt_class == self.belt_select.v_model]
-            .groupby(["from_lc", "to_lc"], as_index=False)
-            .sum()
-        )
-
-        color_dict = pd.read_csv(param.LC_CLASSES)
-
-        color_dict = dict(
-            zip(color_dict.loc[:, "lc_class"], color_dict.loc[:, "color"])
-        )
-
-        output = Output()
-        # rename columns to match with sankey function
-        lbl_left, lbl_right = [
-            str(y) for y in list(self.year_select.v_model.values())[0]
-        ]
-        cols = {"from_lc": lbl_left, "to_lc": lbl_right}
-        self.df_sankey.rename(columns=cols, inplace=True)
-
-        with plt.style.context("dark_background"):
-            with output:
-                output.clear_output()
-                fig, ax = sankey(
-                    self.df_sankey,
-                    colorDict=color_dict,
-                    aspect=4,
-                    rightColor=False,
-                    fontsize=14,
-                )
-                display(fig)
-
-        statistics = sw.Layout(
-            attributes={"id": "render_sub_b"},
-            class_="d-block",
-            children=[
-                sw.CardText(children=[output]),
-            ],
-        )
-
-        self.children = self.children + [statistics]
-
-        self.alert.hide()
+        self.sankey_data.nodes = belt_data["nodes"]
+        self.sankey_data.links = belt_data["links"]
