99def8fd7625f4d7bce15310c0d85ef720881c5b
dfguerrerom <dfgm2006@gmail.com>
2024-10-18 17:58:21 +0200

    add tests

--- component/types.py
+++ component/types.py
@@ -1,5 +1,36 @@
 from pathlib import Path
-from typing import Union
+from typing import Dict, List, NewType, Tuple, TypedDict, Union
 
 Pathlike = Union[str, Path]
 """A type hint for a path-like object, either a string or a Path object."""
+
+
+# Output from reduce regions
+
+
+class Group(TypedDict):
+    lc: int
+    sum: float
+
+
+class SubItem(TypedDict):
+    biobelt: int
+    groups: List[Group]
+
+
+class SubAYearDict(TypedDict):
+    sub_a: List[SubItem]
+
+
+class SubBYearDict(TypedDict):
+    baseline_degradation: List[SubItem]
+    baseline_transition: List[SubItem]
+    final_degradation: List[SubItem]
+    report_transition: List[SubItem]
+
+
+YearKey = NewType("YearKey", str)
+"""YearKey can be either 'singleYear'(sub_A) or 'year__year__year' (sub_B)."""
+
+ResultsDict = Dict[YearKey, Union[SubAYearDict, SubBYearDict]]
+"""A dictionary containing the results of the calculation for subindicators A and/or B."""
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+import component.parameter.module_parameter as param
+
+
 from sepal_ui.scripts.utils import init_ee
 import ee
 import pytest
@@ -7,7 +10,40 @@ from sepal_ui.aoi.aoi_model import AoiModel
 import component.parameter.directory as dir_
 import pandas as pd
 
+from component.scripts.scripts import map_matrix_to_dict
+
 init_ee()
+
+
+@pytest.fixture()
+def default_remap_matrix_a() -> dict:
+    """returns the default remap matrix for subindicator A"""
+    return map_matrix_to_dict(param.LC_MAP_MATRIX)
+
+
+@pytest.fixture()
+def default_remap_matrix_b() -> dict:
+    """returns the default remap matrix for subindicator B"""
+    return map_matrix_to_dict(param.LC_MAP_MATRIX)
+
+
+@pytest.fixture()
+def default_transition_matrix() -> str:
+    return str(param.TRANSITION_MATRIX_FILE)
+
+
+@pytest.fixture()
+def default_dem_asset_id() -> str:
+    """returns dem asset id"""
+
+    return "USGS/SRTMGL1_003"
+
+
+@pytest.fixture()
+def default_dem_asset(default_dem_asset_id) -> ee.Image:
+    """returns dem asset"""
+
+    return ee.Image(default_dem_asset_id)
 
 
 @pytest.fixture()
@@ -15,6 +51,46 @@ def test_aoi() -> ee.FeatureCollection:
     """returns aoi"""
 
     return ee.FeatureCollection("projects/ee-cheprotich22/assets/Argentina_Bounds")
+
+
+@pytest.fixture()
+def test_antioquia_aoi() -> ee.FeatureCollection:
+    """returns a regional aoi (for quick testing)"""
+
+    fc = ee.FeatureCollection("FAO/GAUL/2015/level1")
+    return fc.filter(ee.Filter.eq("ADM1_CODE", 935))
+
+
+@pytest.fixture()
+def test_realsurfacearea_aoi() -> ee.FeatureCollection:
+    """returns a an aoi for real surface area calculation"""
+
+    return ee.FeatureCollection("users/dfgm2006/FAO/MGCI/AOI_RSA")
+
+
+@pytest.fixture()
+def test_sub_a_year():
+    return {
+        1: {"asset": "users/amitghosh/sdg_module/esa/cci_landcover/2000", "year": 2000}
+    }
+
+
+@pytest.fixture()
+def test_sub_b_year():
+    return {
+        "baseline": {
+            "base": {
+                "asset": "users/amitghosh/sdg_module/esa/cci_landcover/2000",
+                "year": 2000,
+            },
+            "report": {
+                "asset": "users/amitghosh/sdg_module/esa/cci_landcover/2015",
+                "year": 2015,
+            },
+        },
+        2: {"asset": "users/amitghosh/sdg_module/esa/cci_landcover/2018", "year": 2018},
+        3: {"asset": "users/amitghosh/sdg_module/esa/cci_landcover/2021", "year": 2021},
+    }
 
 
 @pytest.fixture()
